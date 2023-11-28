bbc41fd2e5d5267a868e2bc4c141174ce5106f56
Your GitHub Username <your-email@example.com>
2023-11-28 20:54:30 +0200

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {TextField , Button} from '@material-ui/core/';
 
 function App() {
   return (
     <div className="App">
+<body>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
       </header>
+    
+      <main>
+
+
+
+        <h1>  הרשמה לאתר  </h1>
+
+      <div className="login">
+      <form>
+      <TextField
+        id="username"
+        label="שם משתמש"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        color="secondary" 
+        
+  
+      />
+      <TextField
+        id="email"
+        label="כתובת אימייל"
+        type="email"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+        color="secondary" 
+        
+        
+      />
+    </form>
+
+    </div>
+
+    <Button variant="contained" color="secondary" size='large' > כניסה </Button>
+    
+      </main>
+
+
+
+
+
+
+
+
+      </body>
     </div>
   );
 }
