3726d4a30ed0d118515e2df9b28d3a3df464425d
Your GitHub Username <your-email@example.com>
2024-01-17 14:29:12 +0200

    add

--- src/components/Lyout_Comp/AppBar_Comp.jsx
+++ src/components/Lyout_Comp/AppBar_Comp.jsx
@@ -24,7 +24,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ffc107",
+      main: "rgb(64, 29, 190)",
     },
   },
 });
